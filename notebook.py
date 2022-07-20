93566fa26aec5c31ed0c2d7e09c012f623aceded
leandelgado <100884489+leandelgado@users.noreply.github.com>
2022-07-20 16:44:06 -0300

    Update notebook.py

--- notebook.py
+++ notebook.py
@@ -1,6 +1,9 @@
 # Introduction
-# Lego is a household name across the world, supported by a diverse toy line, hit movies, and a series of successful video games. In this project, we are going to explore a key development in the history of Lego: the introduction of licensed sets such as Star Wars, Super Heroes, and Harry Potter.
-# It may not be widely known, but Lego has had its share of ups and downs since its inception in the early 20th century. This includes a particularly rough period in the late 90s. Lego was only able to survive due to a successful internal brand (Bionicle) and the introduction of its first licensed series: Star Wars. There are two questions you will need to answer to complete this project.
+# Lego is a household name across the world, supported by a diverse toy line, hit movies, and a series of successful video games. In this project, we are going to 
+# explore a key development in the history of Lego: the introduction of licensed sets such as Star Wars, Super Heroes, and Harry Potter.
+# It may not be widely known, but Lego has had its share of ups and downs since its inception in the early 20th century. This includes a particularly rough period 
+# in the late 90s. Lego was only able to survive due to a successful internal brand (Bionicle) and the introduction of its first licensed series: Star Wars. There 
+# are two questions you will need to answer to complete this project.
 # Before diving into our analysis though, let's become familiar with the two datasets that will help you with this project:
 
 #   datasets/lego_sets.csv:
@@ -18,13 +21,16 @@
 #  * name: The name of the parent theme.
 #  * is_licensed: A Boolean column specifying whether the theme is a licensed theme.
    
-# You are a Data Analyst at Lego working with the Sales/Customer Success teams. The Account Executive responsible for the Star Wars partnership has asked for specific information in preparation for their meeting with the Star Wars team. Although Star Wars was critical to the survival of the brand, Lego has since introduced a wide variety of licensed sets over subsequent years.
+# You are a Data Analyst at Lego working with the Sales/Customer Success teams. The Account Executive responsible for the Star Wars partnership has asked for 
+# specific information in preparation for their meeting with the Star Wars team. Although Star Wars was critical to the survival of the brand, Lego has since 
+# introduced a wide variety of licensed sets over subsequent years.
 
 # Your two questions are as follows:
 
 # 1. What percentage of all licensed sets ever released were Star Wars themed? Save your answer as a variable the_force in the form of an integer (e.g. 25).
 
-# 2. In which year was Star Wars not the most popular licensed theme (in terms of number of sets released that year)? Save your answer as a variable new_era in the form of an integer (e.g. 2012).
+# 2. In which year was Star Wars not the most popular licensed theme (in terms of number of sets released that year)? Save your answer as a variable new_era 
+# in the form of an integer (e.g. 2012).
 
 
 import pandas as pd
